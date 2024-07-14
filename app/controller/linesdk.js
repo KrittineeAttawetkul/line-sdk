c8657e08176c0ec6b8969064d171885bae040bd7
wanthanai kaiphet <wanthanai.bass@nilecon.com>
2024-07-14 22:10:01 +0700

    update services

--- app/controller/linesdk.js
+++ app/controller/linesdk.js
@@ -5,9 +5,9 @@ const LineSDK = require('../model/linesdk');
 exports.Webhook = async function(req, res)
 {
     await LineSDK.Webhook(() => {
-        res.status(200).end(); 
+        res.status(200).send(); 
     }) 
     .catch(() => {
-        res.status(500).end();
+        res.status(500).send();
     })
 }
