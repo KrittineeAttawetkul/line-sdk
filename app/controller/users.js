85a9ff40cbc4c48e686cd9bf4e134bceaae427ee
Krittinee <krittinee.atta@gmail.com>
2024-07-18 15:07:50 +0700

    user.js

--- app/controller/users.js
+++ app/controller/users.js
@@ -2,6 +2,7 @@ const Users = require('../model/users');
 
 exports.getUserByUserId = async function (req, res) {
     const user_id = req.body.user_id
+    console.log('user_id: ', user_id);
     await Users.getUserByUserId(user_id)
     .then((response) => {
         res.status(response.statusCode).send(response)
