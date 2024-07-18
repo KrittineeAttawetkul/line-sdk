af7337bb66c911f521e8ebe48dc7beddc5b14c41
Krittinee <krittinee.atta@gmail.com>
2024-07-18 12:10:24 +0700

    update user.js

--- app/controller/users.js
+++ app/controller/users.js
@@ -2,20 +2,26 @@ const Users = require('../model/users');
 
 exports.getUserByUserId = async function (req, res) {
     const user_id = req.body.user_id
-    try {
-        await Users.getUserByUserId(user_id)
-            .then((response) => {
-                res.status(response.statusCode).send(response)
-            }).catch((err) => {
-                res.status(400).send(err)
-            });
-    }
-    catch (err) {
-        let response = {
-            status: false,
-            errMsg: 'err'
-        }
-        res.status(500).send(response)
-    }
+    await Users.getUserByUserId(user_id)
+    .then((response) => {
+        res.status(response.statusCode).send(response)
+    }).catch((err) => {
+        res.status(400).send(err)
+    });
+    // try {
+    //     await Users.getUserByUserId(user_id)
+    //         .then((response) => {
+    //             res.status(response.statusCode).send(response)
+    //         }).catch((err) => {
+    //             res.status(400).send(err)
+    //         });
+    // }
+    // catch (err) {
+    //     let response = {
+    //         status: false,
+    //         errMsg: 'err'
+    //     }
+    //     res.status(500).send(response)
+    // }
 
 }
