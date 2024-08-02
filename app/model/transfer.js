99df3117345a30b1a485c050b943b3f89aaa4fa7
Krittinee <krittinee.atta@gmail.com>
2024-08-02 13:58:35 +0700

    transfer

--- app/model/transfer.js
+++ app/model/transfer.js
@@ -53,6 +53,14 @@ Transfer.getBalanceByUserId = function (user_id) {
                     UNION ALL
 
                     SELECT 
+                        sender_id AS user_id, 
+                        -point_amount AS point_amount, 
+                        type
+                    FROM point_transfer
+                    WHERE type = 'void'
+                    UNION ALL
+
+                    SELECT 
                         receiver_id AS user_id, 
                         point_amount AS point_amount, 
                         type
@@ -89,6 +97,98 @@ Transfer.getBalanceByUserId = function (user_id) {
         } catch (err) {
             reject(err)
         }
+    })
+}
+
+Transfer.transferPoint = function (transferInput) {
+    return new Promise(async (resolve, reject) => {
+        let response = {
+            status: true,
+            errMsg: '',
+            data: [],
+            statusCode: 200
+        }
+
+        let pointDb = {
+            sender_id:transferInput.sender_id,
+            receiver_id:transferInput.receiver_id,
+            type:'transfer',
+            point_amount:transferInput.point_amount,
+            comment:transferInput.comment,
+            slip_url:transferInput.slip_url
+        }
+
+        await Transfer.getBalanceByUserId(pointDb.sender_id)
+            .then(result => {
+                let balanceData = result.data;
+                console.log(pointDb.sender_id, '-> Balance:', balanceData.balance)
+
+                if (pointDb.sender_id !== pointDb.receiver_id) {
+                    console.log('diff user name')
+                    // Assuming balanceData contains a field named 'balance' that holds the balance value
+                    if (balanceData.balance > 0) {
+                        // Continue with the transfer logic if balance is greater than 0
+
+                        // For example, check if point_amount to be transferred is less than or equal to the balance
+                        if (balanceData.balance >= pointDb.point_amount) {
+
+                            const transfer =
+                                `INSERT INTO point_transfer SET ?`
+
+                            sql.query(
+                                transfer,
+                                [pointDb], // ค่าที่รับเข้ามาเพื่อทำการค้นหา
+                                //call back function
+                                (err, results, fields) => { //results ที่ได้เป็นรูปแบบของ Array
+                                    if (!err) {
+                                        if (results.affectedRows > 0) {
+                                            // If successful
+                                            response.data = { message: 'Transfer successful' };
+                                            resolve(response);
+                                        } else {
+                                            // is not effective
+                                            response.status = false;
+                                            response.errMsg = 'บันทึกไม่สำเร็จ'
+                                            resolve(response);
+                                        }
+                                    } else {
+                                        response["status"] = false;
+                                        response.errMsg = err;
+                                        reject(response);
+                                    }
+                                }
+                            )
+                        } else {
+                            // Insufficient balance
+                            response.status = false;
+                            response.errMsg = 'Insufficient balance';
+                            response.statusCode = 400;
+                            reject(response);
+                        }
+                    } else {
+                        // Balance is not greater than 0
+                        response.status = false;
+                        response.errMsg = 'Balance is not sufficient';
+                        response.statusCode = 400;
+                        reject(response);
+                    }
+                }
+                else {
+                    console.log('same user name')
+                    response.status = false;
+                    response.errMsg = 'Same user name';
+                    response.statusCode = 400;
+                    reject(response);
+                }
+
+
+            })
+            .catch(err => {
+                response.status = false;
+                response.errMsg = err.message || 'Error occurred';
+                response.statusCode = 500;
+                reject(response);
+            });
     })
 }
 
--- app/routes/route.js
+++ app/routes/route.js
@@ -13,5 +13,6 @@ module.exports = function(app){
   app.route("/webhook").post(lineSdk.Webhook);
   // app.route("/api/users/:user_id").get(Users.getUserByUserId);
   app.route("/api/users").post(Users.getUserByUserId);
-  app.route("/api/transfer").post(Transfer.getBalanceByUserId);
+  app.route("/api/balance").post(Transfer.getBalanceByUserId);
+  app.route("/api/transfer").post(Transfer.transferPoint);
 }
