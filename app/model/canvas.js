f4aaac8a6deb2fa409a5d120d355805fb7649d98
Krittinee <krittinee.atta@gmail.com>
2024-08-05 15:59:57 +0700

    Canvas Transfer

--- app/model/canvas.js
+++ app/model/canvas.js
@@ -1,9 +1,8 @@
 const sql = require('../../configs/db');
 const fs = require('fs')
-const { createCanvas, loadImage } = require('canvas')
+const { createCanvas, loadImage, registerFont } = require('canvas')
 const path = require('path');
 const LINE_SDK = require('./lineWebhook');
-
 
 var Canvas = function () {
     this.created_at = new Date()
@@ -59,38 +58,44 @@ Canvas.transferSlip = function (transferInput) {
             const timestamp = `${day}-${month}-${year}_${hours}.${minutes}.${seconds}`;
             const timestampTH = `${day} ${monthTH} ${year + 543} ${hours}:${minutes} น.`;
 
+            // Register the custom font
+            const fontPrompt = path.join(__dirname, '../../assets/Prompt/Prompt-Bold.ttf');
+            registerFont(fontPrompt, { family: 'Prompt' });
+
             const width = 1040
-            const height = 700
+            const height = 585
             const canvas = createCanvas(width, height)
             const ctx = canvas.getContext('2d')
 
             ctx.fillStyle = '#fff'
             ctx.fillRect(0, 0, width, height)
-
-            ctx.font = 'bold 64px Menlo'
-            ctx.textAlign = 'center'
-            ctx.textBaseline = 'top'
+            // ctx.textAlign = 'center'
+            // ctx.textBaseline = 'top'
 
             const receiver_name = receiver.displayName || 'Default Name';
+            ctx.fillStyle = '#000'
+            ctx.font = '46px Prompt'
+            ctx.fillText(`ให้คะแนน`, 350, 75)
 
             ctx.fillStyle = '#000'
-            ctx.fillText(`ให้คะแนน ${receiver_name}`, 520, 35)
+            ctx.font = '46px Sans'
+            ctx.fillText(`${receiver_name}`, 530, 75)
 
-            ctx.fillStyle = '#000'
-            ctx.font = 'bold 80px Menlo'
-            ctx.fillText(`${transferData.point_amount} Point`, 520, 200)
+            ctx.fillStyle = '#555555'
+            ctx.font = '36px Prompt'
+            ctx.fillText(`${transferData.point_amount} Point`, 63, 180)
 
-            ctx.fillStyle = '#000'
-            ctx.font = 'light 50px Menlo'
-            ctx.fillText(`${timestampTH}`, 300, 620)
+            ctx.fillStyle = '#555555'
+            ctx.font = '36px Prompt'
+            ctx.fillText(`${timestampTH}`, 63, 540)
 
             // const buffer = canvas.toBuffer('image/png')
             // fs.writeFileSync('./output.png', buffer)
 
-            ctx.strokeStyle = '#000'
+            ctx.strokeStyle = '#C1C1C1'
             ctx.beginPath()
-            ctx.lineTo(1000, 150)
-            ctx.lineTo(50, 150)
+            ctx.lineTo(1000, 120)
+            ctx.lineTo(50, 120)
             ctx.stroke()
 
             // Function to draw a rounded image
@@ -113,17 +118,17 @@ Canvas.transferSlip = function (transferInput) {
 
             const receiver_img = receiver.pictureUrl
             const sender_img = sender.pictureUrl
-            const arrowPath = path.join(__dirname, '../../assets/Arrow.png');
+            const arrowPath = path.join(__dirname, '../../assets/transferArrow.png');
 
             Promise.all([loadImage(receiver_img), loadImage(sender_img), loadImage(arrowPath)])
                 .then(([receiver_img, sender_img, arrowImg]) => {
 
                     // Draw the image onto the canvas
-                    drawRoundedImage(ctx, receiver_img, 250, 350, 200, 200);
+                    drawRoundedImage(ctx, receiver_img, 338, 210, 236, 236);
 
-                    drawRoundedImage(ctx, sender_img, 600, 350, 200, 200);
+                    drawRoundedImage(ctx, sender_img, 762.65, 210, 236, 236);
 
-                    ctx.drawImage(arrowImg, 470, 420, 100, 40);
+                    ctx.drawImage(arrowImg, 620, 310, 100, 40);
 
                     // Save the canvas to a file
                     const out = fs.createWriteStream('TransferSlip.png');
@@ -132,11 +137,11 @@ Canvas.transferSlip = function (transferInput) {
                     out.on('finish', () => console.log('The PNG file was created.'));
 
                     // Save the canvas to a file with the current date as the name
-                    const outPath = path.join(outputDir, `sender_${timestamp}.png`);
-                    const outFile = fs.createWriteStream(outPath);
-                    const streamFile = canvas.createPNGStream();
-                    streamFile.pipe(outFile);
-                    outFile.on('finish', () => console.log(`The PNG file was created at ${outPath}`));
+                    // const outPath = path.join(outputDir, `sender_${timestamp}.png`);
+                    // const outFile = fs.createWriteStream(outPath);
+                    // const streamFile = canvas.createPNGStream();
+                    // streamFile.pipe(outFile);
+                    // outFile.on('finish', () => console.log(`The PNG file was created at ${outPath}`));
                     resolve()
                 })
                 .catch((err) => {
